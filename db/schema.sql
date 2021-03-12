c173fa7c381a13b8b1768edcf62809f187ba5a51
Just1lz <exelent.pride@rambler.ru>
2021-03-13 00:55:26 +0700

    some edit

--- db/schema.sql
+++ db/schema.sql
@@ -2,16 +2,18 @@ create table if not exists item(
 id serial primary key,
 description text,
 created text,
-done boolean
+done boolean default false
 );
 
-insert into item(description, created, done) VALUES ('task1', '15:05:03  01.01.2021', false);
-insert into item(description, created, done) VALUES ('task2', '15:05:03  02.01.2021', false);
-insert into item(description, created, done) VALUES ('task3', '15:05:03  03.01.2021', false);
-insert into item(description, created, done) VALUES ('task4', '15:05:03  04.01.2021', false);
-insert into item(description, created, done) VALUES ('task5', '15:05:03  05.01.2021', false);
-insert into item(description, created, done) VALUES ('task6', '15:05:03  06.01.2021', false);
-insert into item(description, created, done) VALUES ('task7', '15:05:03  07.01.2021', false);
-insert into item(description, created, done) VALUES ('task8', '15:05:03  08.01.2021', false);
+insert into item(description, created) VALUES ('task1', '15:05:03  01.01.2021');
+insert into item(description, created) VALUES ('task2', '15:05:03  02.01.2021');
+insert into item(description, created) VALUES ('task3', '15:05:03  03.01.2021');
+insert into item(description, created) VALUES ('task4', '15:05:03  04.01.2021');
+insert into item(description, created) VALUES ('task5', '15:05:03  05.01.2021');
+insert into item(description, created) VALUES ('task6', '15:05:03  06.01.2021');
+insert into item(description, created) VALUES ('task7', '15:05:03  07.01.2021');
+insert into item(description, created) VALUES ('task8', '15:05:03  08.01.2021');
+insert into item(description, created) VALUES ('task9', '15:05:03  08.01.2021');
+insert into item(description, created) VALUES ('task10', '15:05:03  08.01.2021');
 
 
--- src/main/java/ru/job4j/todo/model/Item.java
+++ src/main/java/ru/job4j/todo/model/Item.java
@@ -1,4 +1,91 @@
 package ru.job4j.todo.model;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
+
 public class Item {
+    private int id;
+    private String description;
+    private final Date date = new Date();
+    private final String created = changeFormatDate();
+    private boolean done;
+
+    public String changeFormatDate() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss  dd.MM.yyyy");
+        return dateFormat.format(created);
+    }
+
+    public Item() {
+    }
+
+    public Item(String description, boolean done) {
+        this.description = description;
+        this.done = done;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Item item = (Item) o;
+        return Objects.equals(description, item.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(description);
+    }
+
+    @Override
+    public String toString() {
+        return "Item{"
+                + "id="
+                + id
+                + ", description='"
+                + description
+                + '\''
+                + ", created='"
+                + created
+                + '\''
+                + ", done="
+                + done
+                + '}';
+    }
 }
